2ce07a186df404f843ed4a6baef2c45ed3a87af9
Alex <atscott@ucsc.edu>
2021-03-01 09:41:21 -0800

    alexthescott.space homepage

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './Home';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<ome />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
